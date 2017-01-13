2a8d249bfe8fa5afcae123d7f828417da64a3a39
Alina <alina@localhost.localdomain>
2017-01-13 09:48:33 +0100

    fixed final.csv

--- MMSR_Project_ImagerRetrieval/src/scripts/finalProduct/R/tweakSimilarity.R
+++ MMSR_Project_ImagerRetrieval/src/scripts/finalProduct/R/tweakSimilarity.R
@@ -6,17 +6,19 @@ monumentList <- list.dirs(path="/home/alina/Documents/Universität/2016W/Multime
 
 finalList <- NULL
 for (monument in monumentList) {
-  imgVisMonument <- file[file[1]==monument,]
+  monList <- file[file[1]==monument,2]
+  monList <- unique(monList)
   
-  if (nrow(imgVisMonument) > 0) {
+  if (length(monList) > 0) {
     score <- NULL
-    scoreInterval <- 1/(nrow(imgVisMonument)+1)
-    for (i in 1:nrow(imgVisMonument)) {
+    scoreInterval <- 1/(length(monList)+1)
+    for (i in 1:length(monList)) {
       score[i] <- 1-i*scoreInterval
     }
-    finalList <- rbind(finalList, cbind(imgVisMonument[,1], imgVisMonument[,2], score))
+    finalList <- rbind(finalList, cbind(monument, monList, score))
   }
 }
+
 
 write.table(finalList, 
             "/home/alina/Documents/Universität/2016W/Multimedia Search and Retrieval/Project/MMSearch-Retrieval_2016_MainProject/MMSR_Project_ImagerRetrieval/src/scripts/finalProduct/final.csv",
